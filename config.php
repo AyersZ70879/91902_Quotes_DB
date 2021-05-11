3eee68167d48e7708c67b7b9a1fb529cc37c791e
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2021-05-11 14:27:00 +1200

    Finished show all part 1 video

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 
 ini_set('display_errors', 'On');  // change to 'Off' when live
 
-define ("DB_NAME","editme"); 
-define ("DB_USERNAME","editme");
+define ("DB_NAME","ayersz70879_L3_quotes_prac"); 
+define ("DB_USERNAME","ayersz70879");
 define ("DB_PASSWORD","editme"); 
 define ("DB_HOST","localhost");
 
