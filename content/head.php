87d92689721cc03edd6997f4abeaafbe684a1465
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2021-05-11 14:15:39 +1200

    Completed includes video

---
+++ content/head.php
@@ -0,0 +1,19 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="This website displays famous quotes">
+    <meta name="keywords" content="quotes, famous quotes, authors, quote">
+    <meta name="author" content="Zarah Ayers">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Famous Quotes</title>
+    
+    <!-- Edit the link below / replace with your chosen google font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
+    
+    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
+    <link rel="stylesheet" href="css/quick_quotes.css"> 
+        <link rel="stylesheet" href="css/font-awesome.min.css.css">
+    
+</head>
+    
