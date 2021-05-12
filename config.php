2e9f851ac12e248602560c9140261412f5beae33
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2021-05-12 13:01:35 +1200

    Connected to database

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 'On');  // change to 'Off' when live
 
 define ("DB_NAME","ayersz70879_L3_quotes_prac");
 define ("DB_USERNAME","_ayersz70879");
-define ("DB_PASSWORD","152e5b4d4eb6e5133e416a17ed067e68");
+define ("DB_PASSWORD","fXg8EkztxayYrylH");
 define("DB_HOST", "localhost");
 
 ?>
