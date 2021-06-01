df1554f28f79c7acb7014d32fae4f0ae2b934eec
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2021-06-01 14:31:22 +1200

    finished log in part 2 video and website works as expected

--- admin/login.php
+++ admin/login.php
@@ -1,7 +1,16 @@
-<form action="index.php?page=../admin.adminlogin" method="post">
+<form action="index.php?page=../admin/adminlogin" method="post">
 
     <p>Username: <input name="username"/></p>
     <p>Password: <input name="password" type="password" /> </p>
+
+    <?php
+if(isset($_GET['error'])) {
+    ?>
+    <span class="error"><?php echo $_GET['error'] ?> </span>
+
+    <?php
+}
+?>
 
     <p><input type="submit" name="login" value="Log In" /> </p>
 
