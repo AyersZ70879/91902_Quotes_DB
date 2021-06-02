0338201097e5f8e700eaafc1978d653a7abe2a80
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2021-06-02 12:53:36 +1200

    0:21 through new quote part 2 video

--- admin/new_quote.php
+++ admin/new_quote.php
@@ -4,6 +4,15 @@
 if (isset($_SESSION['admin'])) {
     echo "you are logged in";
 
+    // get authors from database 
+    $all_authors_sql = "SELECT * FROM `author` ORDER BY `Last` ASC ";
+    $all_authors_query = mysqli_query($dbconnect, $all_authors_sql);
+    $all_authors_rs = mysqli_fetch_assoc($all_authors_query);
+
+    // initialise author form
+
+    // end user logged in if
+
 } // end user logged in if
 
 else {
