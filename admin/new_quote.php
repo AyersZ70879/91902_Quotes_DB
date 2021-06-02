d68ab8226e436c0cae595b8ec33bd41276df00fa
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2021-06-03 11:56:13 +1200

    Finshed new Quote part 2 video

--- admin/new_quote.php
+++ admin/new_quote.php
@@ -2,7 +2,6 @@
 
 // check user is logged in... 
 if (isset($_SESSION['admin'])) {
-    echo "you are logged in";
 
     // get authors from database 
     $all_authors_sql = "SELECT * FROM `author` ORDER BY `Last` ASC ";
@@ -10,8 +9,10 @@ if (isset($_SESSION['admin'])) {
     $all_authors_rs = mysqli_fetch_assoc($all_authors_query);
 
     // initialise author form
+    $first = "";
+    $middle = "";
+    $last = "";
 
-    // end user logged in if
 
 } // end user logged in if
 
@@ -23,3 +24,53 @@ else {
 } // end user not logged in else
 
 ?>
+<h1>Add a Quote</h1>
+<p><i>
+    Too add a quote, first select the author, then press the 'next' button. If the author is
+    not in the list, please choose the 'New Author' option. To quickly find an author, click
+    in the box below and start typing their <b>first</b> name. 
+</i></p>
+
+<form method="post" enctype="multipart/form-data" action="action=<?php 
+echo htmlspecialchars($_SERVER["PHP_SELF"]."?page=../admin/new_quote");?>">
+
+    <div>
+        <b>Quote Author:</b> &nbsp;
+
+        <select>
+            <!-- Default option is new author -->
+            <option value="unknown" selected>New Author</option>
+
+            <?php
+            do {
+
+                // get authors full name (last, then first)
+                $author_full = $all_authors_rs['First']."
+                ".$all_authors_rs['Last']." ".$all_authors_rs['Middle'];
+
+            ?>
+
+            <option value="<?php echo $all_authors_rs['Author_ID']; ?>">
+                <?php echo $author_full; ?>
+            </option>
+
+            <?php
+
+
+            } // end of author options 'do'
+
+            while($all_authors_rs=mysqli_fetch_assoc($all_authors_query))
+
+            ?>
+
+        </select>
+
+            &nbsp;
+
+        <input class="short" type="submit" name="quote_author" value="Next..." />
+
+    </div>
+
+</form>
+
+&nbsp;
