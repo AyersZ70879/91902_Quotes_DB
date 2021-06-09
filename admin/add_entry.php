417631ba152400b84e6a5c780c0204c1197bfe54
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2021-06-09 12:37:08 +1200

    Entry adding doesn't work - multiple errors found

--- admin/add_entry.php
+++ admin/add_entry.php
@@ -89,9 +89,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $subjectID_3 = get_ID($dbconnect, 'subject', 'SubjectID', 'Subject', $tag_3);
 
         // add entry to database
-        $addentry_sql = "INSERT INTO `quotes` (`ID`, `Author_ID`, `Quote`, 
-        `Notes`, `Subject1_ID`, `Subject2_ID`, `Subject3_ID`) VALUES 
-        (NULL, '$author_ID', '$quote', '$notes', '$subjectID_1', '$subjectID_2', '$subjectID_3')";
+        $addentry_sql = 
+        "INSERT INTO `quotes` (`ID`, `Author_ID`, `Quote`, `Notes`, `Subject1_ID`, `Subject2_ID`, `Subject3_ID`) VALUES (NULL, '$author_ID', '$quote', '$notes', '$subjectID_1', '$subjectID_2', '$subjectID_3')";
         $addentry_query = mysqli_query($dbconnect, $addentry_sql);
 
         // get wuote ID for next page
