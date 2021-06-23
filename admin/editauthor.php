bbb65b922f00a5a708d568b1e7fe9863a8bf38de
Zarah70879 <51766656+Zarah70879@users.noreply.github.com>
2021-06-24 11:25:13 +1200

    Error in editauthor file when trying to edit the author

--- admin/editauthor.php
+++ admin/editauthor.php
@@ -5,20 +5,18 @@ if (isset($_SESSION['admin'])) {
 
     $author_ID = $_REQUEST['authorID'];
 
-     // get country & occupation lists from database
-     $all_countiers_sql="SELECT * FROM `country` ORDER BY `Country` ASC ";
-     $all_countries = autocomplete_list($dbconnect, $all_countiers_sql, 'Country');
- 
-     $all_occupations_sql="SELECT * FROM `career`ORDER BY `Career` ASC ";
-     $all_occupations = autocomplete_list($dbconnect, $all_occupations_sql, 'Career');
- 
- 
- 
+    // get country & occupation lists from database
+    $all_countiers_sql="SELECT * FROM `country` ORDER BY `Country` ASC ";
+    $all_countries = autocomplete_list($dbconnect, $all_countiers_sql, 'Country');
+
+    $all_occupations_sql="SELECT * FROM `career`ORDER BY `Career` ASC ";
+    $all_occupations = autocomplete_list($dbconnect, $all_occupations_sql, 'Career');
+
     $all_authors_sql = "SELECT * FROM `author` WHERE Author_ID = $author_ID";
     $all_authors_query = mysqli_query($dbconnect, $all_authors_sql);
     $all_authors_rs = mysqli_fetch_assoc($all_authors_query);
 
-    // initialise author variables
+    // // initialise author variables
     $first = $all_authors_rs['First'];
     $middle = $all_authors_rs['Middle'];
     $last = $all_authors_rs['Last'];
@@ -52,61 +50,102 @@ if (isset($_SESSION['admin'])) {
     $occupation_2_rs = get_rs($dbconnect, "SELECT * FROM `career` WHERE 
     `Career_ID` = $occupation_2_ID");
 
+    $country_1 = $country_1_rs['Country'];
+    $country_2 = $country_2_rs['Country'];
+    $occupation_1 = $occupation_1_rs['Career'];
+    $occupation_2 = $occupation_2_rs['Career'];
 
-// set up error fields / visibility
-$quote_error = $tag_1_error = "no-error";
-$quote_field = "form-ok";
-$tag_1_field = "tag-ok";
+    // set up error fields / visibility
+    $last_error = $yob_error = $gender_error = $country_1_error = 
+    $occupation_1_error = "no-error";
+    $last_field = $yob_field = $gender_field = "form-ok";
+    $country_1_field = $occupation_1_field = "tag-ok";
 
 // Code below excutes when the form is submitted...
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // get data from form
-    $author_ID = mysqli_real_escape_string($dbconnect, $_POST['author']);
-    $quote = mysqli_real_escape_string($dbconnect, $_POST['quote']);
-    $notes = mysqli_real_escape_string($dbconnect, $_POST['notes']);
-    $tag_1 = mysqli_real_escape_string($dbconnect, $_POST['Subject_1']);
-    $tag_2 = mysqli_real_escape_string($dbconnect, $_POST['Subject_2']);
-    $tag_3 = mysqli_real_escape_string($dbconnect, $_POST['Subject_3']);
+    //  if author is unknown, get values from author part of form
+    
+    $first = mysqli_real_escape_string($dbconnect, $_POST['first']);
+    $middle = mysqli_real_escape_string($dbconnect, $_POST['middle']);
+    $last = mysqli_real_escape_string($dbconnect, $_POST['last']);
+    $yob = mysqli_real_escape_string($dbconnect, $_POST['yob']);
 
-    // check data is valid
-
-    // check quote is not blank
-    if ($quote == "Please type your quote here" || $quote == "") {
-        $has_errors = "yes";
-        $quote_error = "error-text";
-        $quote_field = "form-error";
+    $gender_code = mysqli_real_escape_string($dbconnect, $_POST['gender']);
+    if ($gender_code=="F") {
+        $gender = "Female";
+    }
+    else if ($gender_code=="M") {
+        $gender = "Male";
     }
 
-    if ($tag_1 == "") {
-        $has_errors = "yes";
-        $tag_1_error = "error-text";
-        $tag_1_field = "tag-error";
+    else {
+        $gender = "";
     }
+
+    $country_1 = mysqli_real_escape_string($dbconnect, $_POST['country1']);
+    $country_2 = mysqli_real_escape_string($dbconnect, $_POST['country2']);
+    $occupation_1 = mysqli_real_escape_string($dbconnect, $_POST['occupation1']);
+    $occupation_2 = mysqli_real_escape_string($dbconnect, $_POST['occupation2']);
+
+    // Error checking goes here
+    
+    // check last name is not blank
+    if ($last == "") {
+        $has_errors = "yes";
+        $last_error = "error-text";
+        $last_field = "tag-error";
+    }
+
+    // check year of birth is valid
+    $valid_yob = isValidYear($yob);
+
+    if($yob < 0 || $valid_yob != 1 || !preg_match('/^\d{1,4}$/', $yob))
+    {
+        $has_errors = "yes";
+        $yob_error = "error-text";
+        $yob_field = "form-error";
+    }
+
+    // check first country is not blank
+    if ($country_1 == "") {
+        $has_errors = "yes";
+        $country_1_error = "error-text";
+        $country_1_field = "tag-error";
+    }
+
+    // check occupation is not blank
+    if ($occupation_1 == "") {
+        $has_errors = "yes";
+        $occupation_1_error = "error-text";
+        $occupation_1__field = "tag-error";
+    }
+
 
     if($has_errors != "yes") {
 
-        // Get subject ID's via get_ID function
-        $subjectID_1 = get_ID($dbconnect, 'subject', 'Subject_ID', 'Subject', $tag_1);
-        $subjectID_2 = get_ID($dbconnect, 'subject', 'Subject_ID', 'Subject', $tag_2);
-        $subjectID_3 = get_ID($dbconnect, 'subject', 'Subject_ID', 'Subject', $tag_3);
+        
+        // get country and occupation IDs
+        $countryID_1 = get_ID($dbconnect, 'country', 'Country_ID', 'Country', $country_1);
+        $countryID_2 = get_ID($dbconnect, 'country', 'Country_ID', 'Country', $country_2);
 
-        // edit database entry
-        $editentry_sql = "UPDATE `quotes` SET `Author_ID` = '$author_ID', `Quote` = '$quote', `Notes` 
-        = '$notes', `Subject1_ID` = '$subjectID_1', `Subject2_ID` = '$subjectID_2', 
-        `Subject3_ID` = '$subjectID_3' WHERE `quotes`.`ID` = $ID;";
-        $editentry_query = mysqli_query($dbconnect, $editentry_sql);
+        $occupationID_1 = get_ID($dbconnect, 'career', 'Career_ID', 'Career', $occupation_1);
+        $occupationID_2 = get_ID($dbconnect, 'career', 'Career_ID', 'Career', $occupation_2);
 
-        // get quote ID for next page
-        $get_quote_sql = "SELECT * FROM `quotes` WHERE `Quote` = '$quote'";
-        $get_quote_query = mysqli_query($dbconnect, $get_quote_sql);
-        $get_quote_rs = mysqli_fetch_assoc($get_quote_query);
 
-        $quote_ID = $get_quote_rs['ID'];
-        $_SESSION['Quote_Success']=$quote_ID;
-
-        // Go to success page...
-        header('Location: index.php?page=editquote_success&quote_ID='.$quote_ID);
+        // add author to database
+        $editauthor_sql = "UPDATE `author` SET `First` = '$first', `Middle` = '$middle, 
+        `Last` = '$last, `Gender` = '$gender_code', `Born` = '$yob', 
+        `Country1_ID` = '$countryID_1', `Country2_ID` = '$countryID_2, 
+        `Career1_ID` = '$occupationID_1', `Career2_ID` = '$occupationID_2' WHERE 
+        `author`.`Author_ID` = $author_ID;
+        ";
+        $edit_author_query = mysqli_query($dbconnect, $edit_author_sql);
+        
+        
+    
+        // Go to author page... 
+        header('Location: index.php?page=author&authorID='.$author_ID);
 
 
     } // end has errors if
@@ -122,119 +161,123 @@ else {
 
 } // end user not logged in else
 
-// ?>
-
-<h1>Edit Quote...</h1>
+ ?>
+ <h1>Add an Author...</h1>
 
 <form autocomplete="off" method="post" action="<?php 
-echo htmlspecialchars($_SERVER["PHP_SELF"]."?page=../admin/editquote&ID=$ID");?>">
+echo htmlspecialchars($_SERVER["PHP_SELF"]."?page=../admin/editauthor&authorID=$author_ID");?>">
 
-    <p>
-        <i>If you need to change this quotes's author and the author you need is 
-        NOT in the list below. Please <a href="index.php?page=../admin/add_author" target="_blank">add the author</a>. 
-        Then come back and reload this page to refresh the list.</i>
-    </p>
-    
-    &nbsp;
-
-    <select class="adv gender" name="author">
-        <!-- Default option is new author -->
-        <option value="<?php echo $author_ID; ?>" selected>
-        <?php echo $current_author; ?>
-        </option>
-
-        <?php
-
-        // get authors from database
-        $all_authors_sql = "UPDATE `quotes` SET";
-        $all_authors_query = mysqli_query($dbconnect, $all_authors_sql);
-        $all_authors_rs = mysqli_fetch_assoc($all_authors_query);
-
-        do {
-
-            $author_ID = $all_authors_rs['Author_ID'];
-            $first = $all_authors_rs['First'];
-            $middle = $all_authors_rs['Middle'];
-            $last = $all_authors_rs['Last'];
-
-            $author_full = $last." ".$first." ".$middle;
-
+    <?php
+    // fields to add new author information
         ?>
+        <!-- Author's first name, optional -->
+        <input class="add-field" type="text" name="first" value="<?php echo 
+        $first; ?>" placeholder="Author's First Name" />
 
-        <option value="<?php echo $author_ID; ?>">
-            <?php echo $author_full; ?>
-        </option>
+        <br /> <br />
 
-        <?php
+        <input class="add-field" type="text" name="middle" value="<?php echo 
+        $middle ?>" placeholder="Author's Middle Name (optional)" />
 
+        <br /> <br />
 
-        } // end of author options 'do'
+        <div class="<?php echo $last_error; ?>">
+            Author's last name can't be blank
+        </div>
 
-        while($all_authors_rs=mysqli_fetch_assoc($all_authors_query))
+        <input class="add-field" <?php echo $last_field; ?> type="text"
+        name="last" value="<?php echo $last; ?>" placeholder="Author's Last Name"
+        />
 
-        ?>
+        <br /> <br />
 
-    </select>
+        <select class="adv gender <?php echo $gender_field; ?>" name="gender">
 
+            <?php 
+            if($gender_code=="") {
+                ?>
+                <option value="" selected>Gender (Choose something)...
+                </option>
+                <?php
+            } // end gender not chose if
 
-    <!-- Quote entry in add entry - Required -->
-    <div class="<?php echo $quote_error; ?>">
-        This field can't be blank
-    </div>
+            else {
+                ?>
+                    <option value="<?php echo $gender_code; ?>" selected>
+                    <?php echo $gender; ?>
+                    </option>
 
-    <textarea class="add-field <?php echo $quote_field?>" name="quote" rows="6"><?php echo $quote; ?></textarea>
+                <?php
+            } //  end gender chosen else
+            ?>
+            <option value="F">Female</option>
+            <option value="M">Male</option>
+        
+        </select>
 
-    <!-- Notes section in add entry -->
-    <input class="add-field <?php echo $notes; ?>" type="text" name="notes" value="<?php
-    echo $notes; ?>" placeholder="Notes (optional) ..."/>
-    <br /> <br />
+        <br /> <br />
+        
+        <div class="<?php echo $yob_error; ?>">
+            Please enter a valid year of birth (modern author's only).
+        </div>
 
-    <!-- Subject 1 entry in add entry - Required -->
-    <div class="<?php echo $tag_1_error; ?>">
-        Please enter at least one subject tag
-    </div>
-    <div class="autocomplete">
-        <input class="<?php echo $tag_1_field; ?>" id="subject1" type="text" name="Subject_1" 
-        value="<?php echo $tag_1; ?>" placeholder="Subject 1 (Start Typing...)">
-    </div>
+        <input class="add-field <?php echo $yob_field; ?>" type="text" name="yob"
+        value="<?php echo $yob; ?>" placeholder="Author's year of birth" />
 
-     <br /> <br />
+        <br /> <br />
 
-    <!-- Subject 2 entry in add entry -->
-    <div class="autocomplete">
-    
-        <input id="subject2" type="text" name="Subject_2" value="<?php echo $tag_2; ?>"
-        placeholder="Subject 2 (Start Typing, optional)...">
-    </div>
+        <div class="<?php echo $country_1_error; ?>">
+            Please enter at least one country
+        </div>
 
-    <br /> <br />
+        <div class="autocomplete">
+            <input class="<?php $country_1_field; ?>" id="country1" type="text"
+            name="country1" value="<?php echo $country_1; ?>" placeholder="Country 1 (Required, Start Typing)...">
+        </div>
 
-    <!-- Subject 3 entry in add entry -->
-    <div class="autocomplete">
-    
-        <input id="subject3" type="text" name="Subject_3" value="<?php echo $tag_3; ?>"
-        placeholder="Subject 3 (Start Typing, optional)...">
-    </div> 
+        <br /> <br />
 
-    <br /> <br />
+        <div class="autocomplete">
+            <input id="country2" type="text" name="country2" value="<?php echo $country_2; ?>"
+            placeholder="Country 2 (Start Typing)...">
+        </div> 
+
+        <br /> <br />
+
+        <div class="<?php echo $occupation_1_error; ?>">
+            Please enter at least one country
+        </div>
+
+        <div class="autocomplete">
+            <input class="<?php echo $occupation_1_field; ?>" id="occupation1" type="text"
+            name="occupation1" value="<?php echo $occupation_1; ?>" placeholder="Occupation 1 (Required, Start Typing)...">
+        </div>
+        <br /> <br />
+
+        <div class="autocomplete">
+            <input type="text" id="occupation2" name="occupation2" value="<?php echo $occupation_2; ?>"
+            placeholder="Occupation 2 (Start Typing...)">
+        </div>
+
+        <br /> <br />
 
     <!-- Submit Button -->
     <p>
-        <input type="submit" vlaue="Submit" />
+        <input type="submit" value="Submit" />
     </p>
 
-
-</form>
 
 <!-- script to make autocomplete work -->
 <script>
 <?php include("autocomplete.php"); ?>
 
 /* Arrays containing lists */
-var all_tags = <?php print("$all_subjects"); ?>;
-autocomplete(document.getElementById("subject1"), all_tags);
-autocomplete(document.getElementById("subject2"), all_tags);
-autocomplete(document.getElementById("subject3"), all_tags);
+var all_countries = <?php print("$all_countries"); ?>;
+autocomplete(document.getElementById("country1"), all_countries);
+autocomplete(document.getElementById("country2"), all_countries);
 
+var all_occupations = <?php print("$all_occupations"); ?>;
+autocomplete(document.getElementById("occupation1"), all_occupations);
+autocomplete(document.getElementById("occupation2"), all_occupations);
 
 </script>
